9add753a321bc0d1cd6b74bc5087eefcaed537fa
Casie Siekman <casie.siekman@gmail.com>
2023-01-24 13:12:02 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// useSelector is a hook provided by the react-redux library
+import { useSelector } from 'react-redux';
+
+function App () {
+    // useSelector accepts a function that tells it what part of the store you want.
+    // here we return the whole store
+    const reduxStore = useSelector(store => store)
+    const count = useSelector(store => store.count)
+
+
+    return (
+      <div>
+        {/* Render the entire reduxStore to our DOM, as a JS object (JSON) */}
+        <pre>{JSON.stringify(reduxStore)}</pre>
+        <p>Count is: {count}</p>
+
+      </div>
+    );
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// reducer!
+const count = (state = 0, action) => {
+    console.log(`Hey!!! I'm a reducer y'all!!!`);
+
+    return state;
+};
+
+const elementList = (state = [], action) => {
+  return state;
+};
+
+// store!
+const storeInstance = createStore(
+  combineReducers(
+      {
+          count,
+          elementList,
+      }
+  )
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Provider lets redux and react talk to one another
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
